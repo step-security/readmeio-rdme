760d37ea18c9c488c3aadf44c101a4144b4d1634
Anurag Rajawat <anurag@stepsecurity.io>
2026-06-12 13:43:54 +0530

    chore: moving from Biome+Prettier to Oxc (#1438)

--- bin/run.js
+++ bin/run.js
@@ -49,7 +49,8 @@ async function validateSubscription() {
 async function main() {
   await validateSubscription();
   /**
-   * Disables the oclif engine warning. For some reason the versions that are flagged are totally separate from our engines requirements.
+   * Disables the oclif engine warning. For some reason the versions that are flagged are totally
+   * separate from our engines requirements.
    *
    * @see {@link https://github.com/oclif/core/blob/70d3f192862a5adb548cbda68c6ee1ca8f724110/src/index.ts#L12}
    */
@@ -62,11 +63,10 @@ async function main() {
   }
   await execute(opts).then(msg => {
     if (msg && typeof msg === 'string') {
-      // biome-ignore lint/suspicious/noConsole: This is in an executable.
+      // oxlint-disable-next-line no-console -- This is in an executable.
       console.log(msg);
     }
   });
 }
 
-// biome-ignore lint/nursery/noFloatingPromises: We use rollup to bundle this file and it doesn't play well with top-level await.
 main();
